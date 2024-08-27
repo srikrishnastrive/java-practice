c34909121ebcf0c75b9c3279ea3f6931a9beab18
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-08-27 19:21:04 +0530

    added the odd problem

--- strings/problem_3.java
+++ strings/problem_3.java
@@ -1,4 +1,12 @@
-
+/**
+ * Given a string s, representing a large integer, the task is to return the largest-valued odd integer (as a string) that is a substring of the given string s.
+The number returned should not have leading zero's. But the given input string may have leading zero.
+Example 1
+Input : s = "5347"
+Output : "5347"
+Explanation : The odd numbers formed by given strings are --> 5, 3, 53, 347, 5347.
+So the largest among all the possible odd numbers for given string is 5347.
+ */
 class OddNumber{
     public String LargestOddNumber(String s){
         int ind = -1;
